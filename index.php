a791d879b5cd45dc3e435525788803cab83d197b
timmycheng <timmycheng@foxmail.com>
2016-02-29 20:35:44 +0800

    make the php index page

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Xingu Landscape</title>
+    <link href="./index_files/common_o2.1-ff38235b1bb2cca69137d1b7431cf585.css" media="all" rel="stylesheet" type="text/css">
+    <link href="./index_files/main-41d3931e3cd1d40b3538e4ee71e940d0.css" media="screen" rel="stylesheet" type="text/css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="mobile-web-app-capable" content="yes">
+	<meta name="theme-color" content="#f5f5f5">
+</head>
+<body class="wiht-new-header home_view v2 simple-header p1">
+	<div id="header" class="airbnb-header new shift-whit-hiw">
+		<header class="header--sm show-sm" aria-hidden="true" role="banner">
+			<a href="#" aria-label="Index" data-prevent-default="" class="link-reset burger--sm">
+				<i class="icon icon-reorder icon-rausch"></i>
+				<span class="screen-reader-only">
+					Xingu Landscape
+				</span>
+			</a>
+		</header>
+	</div>
+	
+</body>
+</html>
