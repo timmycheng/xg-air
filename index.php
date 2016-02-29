6d0630705d916bb4ef0e47ca3032d31fab8ebb13
tim@home <timmycheng@foxmail.com>
2016-02-29 22:57:11 +0800

    change index.html

--- index.php
+++ index.php
@@ -12,16 +12,100 @@
 	<meta name="theme-color" content="#f5f5f5">
 </head>
 <body class="wiht-new-header home_view v2 simple-header p1">
-	<div id="header" class="airbnb-header new shift-whit-hiw">
-		<header class="header--sm show-sm" aria-hidden="true" role="banner">
-			<a href="#" aria-label="Index" data-prevent-default="" class="link-reset burger--sm">
-				<i class="icon icon-reorder icon-rausch"></i>
-				<span class="screen-reader-only">
-					Xingu Landscape
-				</span>
-			</a>
-		</header>
+	<div id="header" class="airbnb-header new shift-with-hiw">
+		<div class="regular-header regular-header--new clearfix hide-sm" id="new-header">
+			<div class="comp pull-left">
+			  <a href="./index_files/index.html" class="hdr-btn link-reset belo-container" aria-label="Airbnb">
+			    <i class="icon icon-pos--lower icon-white icon-airbnb-alt h2"></i>
+			    <i class="icon icon-pos--lower icon-white icon-airbnb"></i>
+			  </a>
+			</div>
+	
+			<div class="comp pull-right">
+			  <a data-login-modal="" href="https://zh.airbnb.com/login" data-redirect="redirect_params[action]=show&amp;redirect_params[controller]=homepages" class="hdr-btn link-reset">
+			    登录
+			  </a>
+			</div>
+			<div class="comp pull-right">
+			  <a data-signup-modal="" data-header-view="true" href="https://zh.airbnb.com/signup_login" data-redirect="redirect_params[action]=show&amp;redirect_params[controller]=homepages" class="hdr-btn link-reset">
+			    注册
+			  </a>
+			</div>
+		</div>
 	</div>
+	<main id="site-content" role="main">
+	 <div class="hero shift-with-hiw js-hero">
+	  <div class="hero__background" data-native-currency="CNY" aria-hidden="true">
+	    <div class="air-slideshow hero-slideshow">
+	      <ul class="air-slideshow__list list-unstyled js-hero-slideshow">
+	        <li class="air-slide" aria-hidden="true" style="display: none;">
+	          <img alt="Paris-ac2c9c35b05b19e65af7c8eee89f2fae" src="./index_files/paris-ac2c9c35b05b19e65af7c8eee89f2fae.jpg" width="100%">
+	        </li>
+	        <li class="air-slide" aria-hidden="true" style="display: none;">
+	          <img data-image-url=".index_files/hero1-05aa8ee88095f73396a5d8830699c7ea.jpg" alt="" role="presentation" width="100%" src="./index_files/hero1-05aa8ee88095f73396a5d8830699c7ea.jpg">
+	        </li>
+	        <li class="air-slide" aria-hidden="true" style="display: none;">
+	          <img data-image-url=".index_files/hero2-d3f476cd0cd289d1750fa6cd58b9d519.jpg" alt="" role="presentation" width="100%" src="./index_files/hero2-d3f476cd0cd289d1750fa6cd58b9d519.jpg">
+	        </li>
+	        <li class="air-slide" aria-hidden="true" style="display: none;">
+	          <img data-image-url=".index_files/hero3-5799c6b3e1b4e3f714c738a2424410d0.jpg" alt="" role="presentation" width="100%" src="./index_files/hero3-5799c6b3e1b4e3f714c738a2424410d0.jpg">
+	        </li>
+	        <li class="air-slide" aria-hidden="true" style="display: none;">
+	          <img data-image-url=".index_files/hero4-8b0ee569d59e51fdd32457b2ab1dfb11.jpg" alt="" role="presentation" width="100%" src="./index_files/hero4-8b0ee569d59e51fdd32457b2ab1dfb11.jpg">
+	        </li>
+	        <li class="air-slide air-slide--active" aria-hidden="false" style="display: list-item;">
+	          <img data-image-url=".index_files/hero5-70cd04b96bf437d802404f06d9aebe3a.jpg" alt="" role="presentation" width="100%" src="./index_files/hero5-70cd04b96bf437d802404f06d9aebe3a.jpg">
+	        </li>
+	        <li class="air-slide" aria-hidden="true" style="display: none;">
+	          <img data-image-url=".index_files/hero6-1b571925beb84a386bd4873a41992f21.jpg" alt="" role="presentation" width="100%" src="./index_files/hero6-1b571925beb84a386bd4873a41992f21.jpg">
+	        </li>
+	        <li class="air-slide" aria-hidden="true" style="display: none;">
+	          <img data-image-url=".index_files/hero7-c7cd418e4931ac4dfeca68de9c61bb36.jpg" alt="" role="presentation" width="100%" src="./index_files/hero7-c7cd418e4931ac4dfeca68de9c61bb36.jpg">
+	        </li>
+	        <li class="air-slide" aria-hidden="true" style="display: none;">
+	          <img data-image-url=".index_files/hero8-2f00a96727707da3071dd8502113792f.jpg" alt="" role="presentation" width="100%" src="./index_files/hero8-2f00a96727707da3071dd8502113792f.jpg">
+	        </li>
+	        <li class="air-slide" aria-hidden="true" style="display: none;">
+	          <img data-image-url=".index_files/hero9-4e6b1644d27356d57b6c3e1a72fec8fe.jpg" alt="" role="presentation" width="100%" src="./index_files/hero9-4e6b1644d27356d57b6c3e1a72fec8fe.jpg">
+	        </li>
+	      </ul>
+	    </div>
+	  </div>
+
+	  <div class="hero__content page-container page-container-full text-center">
+	    <div class="va-container va-container-v va-container-h">
+	      <div class="va-middle">
+	        <h2 class="text-branding text-jumbo text-contrast space-1">
+	          欢迎回家
+	        </h2>
+	        <div class="h4 text-contrast space-4">
+	          向超过190个国家的当地人租住独一无二的家。
+	        </div>
+
+	        <div class="show-sm sm-search">
+	            <div class="input-addon" id="sm-search-field">
+	    <span class="input-stem input-large fake-search-field">
+	      您想去哪里？
+	    </span>
+	    <i class="input-suffix btn btn-primary icon icon-full icon-search"></i>
+	  </div>
+
+	        </div>
+
+	        <a href="https://zh.airbnb.com/info/how_it_works" class="btn hide-sm btn-contrast btn-large btn-semi-transparent js-show-how-it-works">
+	          如何使用Airbnb
+	        </a>
+
+	      </div>
+
+	    </div>
+	    <!-- <div class="hero__content-footer hide-sm">
+	      <div class="col-sm-12">
+	      </div>
+	    </div> -->
+	  </div>
+	</div>
+	</main>
 	
 </body>
 </html>
